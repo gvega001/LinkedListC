7c10300b3221470a74cc7fd87f1efde4f1b5d52f
gvega001 <gvega001@gmail.com>
2021-02-06 09:22:33 -0800

    Created Insert.

---
+++ insert.cpp
@@ -0,0 +1,11 @@
+﻿#include "insert.h"
+
+struct Node;
+
+struct Node* create_node(int data);
+
+void b_insert(struct  Node** head, int data);
+void n_insert(struct Node** head, int data, int position);
+void e_insert(struct Node** head, int data);
+
+
